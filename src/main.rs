ddd38e32eb2e9c9f0b3e9c4b1c24fbd1d50971b2
Ihar Novik <i.novik@godeltech.com>
2022-07-03 15:29:45 +0300

    multisearch-support - add possibility to search different locations and send updates to different chats.

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use std::thread::sleep;
 use std::time;
 
 const OTODOM_BASE_URL: &str = "https://www.otodom.pl";
-const OTODOM_SEARCH_URL: &str = "https://www.otodom.pl/pl/oferty/wynajem/mieszkanie/lodz";
-const OLX_SEARCH_URL: &str = "https://www.olx.pl/nieruchomosci/mieszkania/wynajem/lodzkie/";
+const OTODOM_SEARCH_URL: &str = "https://www.otodom.pl/pl/oferty/wynajem/mieszkanie/";
+const OLX_SEARCH_URL: &str = "https://www.olx.pl/nieruchomosci/mieszkania/wynajem/";
 const TELEGRAM_SEND_MSG_URL: &str =
     "https://api.telegram.org/{TELEGRAM_BOT_ID}:{TELEGRAM_BOT_TOKEN}/sendMessage";
-const CHAT_ID_PARAM: (&str, &str) = ("chat_id", "{TELEGRAM_CHAT_ID}");
+const CHAT_SEARCH_LINKS: [(&str, &str); 2] = [("lodz", "785197852"), ("wroclaw", "785197852")];
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut handled_links = HashSet::new();
@@ -17,20 +17,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     loop {
         sleep(time::Duration::from_secs(5));
-        handle_otodom_posts(&client, &mut handled_links);
-        handle_olx_posts(&client, &mut handled_links);
+        for chat_tuple in CHAT_SEARCH_LINKS.iter() {
+            handle_otodom_posts(&client, &mut handled_links, chat_tuple);
+            handle_olx_posts(&client, &mut handled_links, chat_tuple);
+        }
     }
 }
 
-fn handle_otodom_posts(client: &Client, handled_links: &mut HashSet<String>) {
-    match get(OTODOM_SEARCH_URL) {
+fn handle_otodom_posts(
+    client: &Client,
+    handled_links: &mut HashSet<String>,
+    chat_tuple: &(&str, &str),
+) {
+    match get(format!("{}{}", OTODOM_SEARCH_URL, chat_tuple.0)) {
         Ok(resp) => {
             let body = resp.text().unwrap();
             let document = Html::parse_document(&body);
             let selector = Selector::parse(r#"a[data-cy="listing-item-link"]"#).unwrap();
             for element in document.select(&selector) {
                 let link = OTODOM_BASE_URL.to_owned() + element.value().attr("href").unwrap();
-                handle_parsed_link(client, handled_links, link);
+                handle_parsed_link(client, handled_links, link, chat_tuple.1);
             }
         }
         Err(err) => {
@@ -39,15 +45,19 @@ fn handle_otodom_posts(client: &Client, handled_links: &mut HashSet<String>) {
     }
 }
 
-fn handle_olx_posts(client: &Client, handled_links: &mut HashSet<String>) {
-    match get(OLX_SEARCH_URL) {
+fn handle_olx_posts(
+    client: &Client,
+    handled_links: &mut HashSet<String>,
+    chat_tuple: &(&str, &str),
+) {
+    match get(format!("{}{}", OLX_SEARCH_URL, chat_tuple.0)) {
         Ok(resp) => {
             let body = resp.text().unwrap();
             let document = Html::parse_document(&body);
             let selector = Selector::parse(r#"a[data-cy="listing-ad-title"]"#).unwrap();
             for element in document.select(&selector) {
                 let link = element.value().attr("href").unwrap();
-                handle_parsed_link(client, handled_links, link.to_string());
+                handle_parsed_link(client, handled_links, link.to_string(), chat_tuple.1);
             }
         }
         Err(err) => {
@@ -56,16 +66,21 @@ fn handle_olx_posts(client: &Client, handled_links: &mut HashSet<String>) {
     }
 }
 
-fn handle_parsed_link(client: &Client, handled_links: &mut HashSet<String>, link: String) {
+fn handle_parsed_link(
+    client: &Client,
+    handled_links: &mut HashSet<String>,
+    link: String,
+    chat_id: &str,
+) {
     if !handled_links.contains(&link) {
         println!("Link:{:#?}", &link);
-        send_link_to_telegram(client, &link);
+        send_link_to_telegram(client, chat_id, &link);
         handled_links.insert(link);
     }
 }
 
-fn send_link_to_telegram(client: &Client, link: &str) {
-    let params = [CHAT_ID_PARAM, ("text", link)];
+fn send_link_to_telegram(client: &Client, chat_id: &str, link: &str) {
+    let params = [("chat_id", chat_id), ("text", link)];
     match client.post(TELEGRAM_SEND_MSG_URL).form(&params).send() {
         Ok(res) => println!("Telegram response:{:#?}", res.status()),
         Err(e) => println!("Error sending message to Telegram: {}", e),
