4342503916459115c7047fd6550e3666fd01b688
Ihar Novik <i.novik@godeltech.com>
2022-07-06 19:37:21 +0300

    add-redis - add redis as persistent storage for handled links.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+extern crate redis;
+
+use redis::{Commands, Connection};
 use reqwest::blocking::{get, Client};
 use scraper::{Html, Selector};
-use std::collections::HashSet;
 use std::thread::sleep;
 use std::time;
 
@@ -12,23 +14,20 @@ const TELEGRAM_SEND_MSG_URL: &str =
 const CHAT_SEARCH_LINKS: [(&str, &str); 2] = [("lodz", "785197852"), ("wroclaw", "785197852")];
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut handled_links = HashSet::new();
+    let redis_client = redis::Client::open("redis://127.0.0.1/")?;
+    let mut redis_con = redis_client.get_connection()?;
     let client = Client::new();
 
     loop {
         sleep(time::Duration::from_secs(5));
         for chat_tuple in CHAT_SEARCH_LINKS.iter() {
-            handle_otodom_posts(&client, &mut handled_links, chat_tuple);
-            handle_olx_posts(&client, &mut handled_links, chat_tuple);
+            handle_otodom_posts(&client, &mut redis_con, chat_tuple);
+            handle_olx_posts(&client, &mut redis_con, chat_tuple);
         }
     }
 }
 
-fn handle_otodom_posts(
-    client: &Client,
-    handled_links: &mut HashSet<String>,
-    chat_tuple: &(&str, &str),
-) {
+fn handle_otodom_posts(client: &Client, redis_con: &mut Connection, chat_tuple: &(&str, &str)) {
     match get(format!("{}{}", OTODOM_SEARCH_URL, chat_tuple.0)) {
         Ok(resp) => {
             let body = resp.text().unwrap();
@@ -36,7 +35,7 @@ fn handle_otodom_posts(
             let selector = Selector::parse(r#"a[data-cy="listing-item-link"]"#).unwrap();
             for element in document.select(&selector) {
                 let link = OTODOM_BASE_URL.to_owned() + element.value().attr("href").unwrap();
-                handle_parsed_link(client, handled_links, link, chat_tuple.1);
+                handle_parsed_link(client, redis_con, link, chat_tuple.1);
             }
         }
         Err(err) => {
@@ -45,11 +44,7 @@ fn handle_otodom_posts(
     }
 }
 
-fn handle_olx_posts(
-    client: &Client,
-    handled_links: &mut HashSet<String>,
-    chat_tuple: &(&str, &str),
-) {
+fn handle_olx_posts(client: &Client, redis_con: &mut Connection, chat_tuple: &(&str, &str)) {
     match get(format!("{}{}", OLX_SEARCH_URL, chat_tuple.0)) {
         Ok(resp) => {
             let body = resp.text().unwrap();
@@ -57,7 +52,7 @@ fn handle_olx_posts(
             let selector = Selector::parse(r#"a[data-cy="listing-ad-title"]"#).unwrap();
             for element in document.select(&selector) {
                 let link = element.value().attr("href").unwrap();
-                handle_parsed_link(client, handled_links, link.to_string(), chat_tuple.1);
+                handle_parsed_link(client, redis_con, link.to_string(), chat_tuple.1);
             }
         }
         Err(err) => {
@@ -66,16 +61,12 @@ fn handle_olx_posts(
     }
 }
 
-fn handle_parsed_link(
-    client: &Client,
-    handled_links: &mut HashSet<String>,
-    link: String,
-    chat_id: &str,
-) {
-    if !handled_links.contains(&link) {
+fn handle_parsed_link(client: &Client, redis_con: &mut Connection, link: String, chat_id: &str) {
+    let is_link_handled: bool = redis_con.exists(&link).expect("failed to check link in redis");
+    if !is_link_handled {
         println!("Link:{:#?}", &link);
         send_link_to_telegram(client, chat_id, &link);
-        handled_links.insert(link);
+        let _: () = redis_con.set(link, 1).expect("failed to add link to redis");
     }
 }
 
